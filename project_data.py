46ebdae4232bac46f9bc1e15f3bac4602ef3cccf
darksapien23151 <141660450+darksapien23151@users.noreply.github.com>
2023-08-19 11:49:34 +0530

    Update project_data.py

--- project_data.py
+++ project_data.py
@@ -1,1 +1,14 @@
+E = {0, 1, 2, 3, 4, 5, 6, 8}
+N = {2, 4, 6, 8, 10}
+
+union = E.union(N)
+intersection = E.intersection(N)
+difference = E.difference(N)
+symmetric_diff = E.symmetric_difference(N)
+
+# Print the results
+print("Union of E and N is {union}")
+print("Intersection of E and N is {intersection}")
+print("Difference of E and N is {difference}")
+print("Symmetric difference of E and N is {symmetric_diff}")
 
